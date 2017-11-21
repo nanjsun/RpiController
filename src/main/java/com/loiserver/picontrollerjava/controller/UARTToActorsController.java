c31a2de8f9b4ac13288de69134420ba3d60635a9
jam <279956327@qq.com>
2017-11-21 15:09:25 +0800

    UART with IOC done, send message set as a varialbe i n url done

--- src/main/java/com/loiserver/picontrollerjava/controller/UARTToActorsController.java
+++ src/main/java/com/loiserver/picontrollerjava/controller/UARTToActorsController.java
@@ -3,6 +3,6 @@ package com.loiserver.picontrollerjava.controller;
 import java.io.IOException;
 
 public interface UARTToActorsController {
-    String sendMessage() throws IOException, InterruptedException;
+    String sendMessage(String messageSendToActor) throws IOException, InterruptedException;
     String recceiveMessage() throws InterruptedException, IOException;
 }
--- src/main/java/com/loiserver/picontrollerjava/controller/UARTToGasSupplierController.java
+++ src/main/java/com/loiserver/picontrollerjava/controller/UARTToGasSupplierController.java
@@ -5,6 +5,7 @@ import com.loiserver.picontrollerjava.model.UARTToGasSupplier;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
@@ -12,7 +13,6 @@ import java.io.IOException;
 @RestController
 @Component
 public class UARTToGasSupplierController implements UARTToActorsController{
-//public class UARTToGasSupplierController{
 
     private UARTToActors uartToActors;
 
@@ -22,50 +22,18 @@ public class UARTToGasSupplierController implements UARTToActorsController{
     }
 
     @RequestMapping("/uartsend")
-    public String sendMessage() throws IOException, NullPointerException, InterruptedException{
-        uartToActors.sendToGasSupplier("see me ?");
+    public String sendMessage(@RequestParam("messageSendToGasSupplier") String messageSendToGasSupplier) throws IOException, NullPointerException, InterruptedException{
+
+        uartToActors.sendToGasSupplier(messageSendToGasSupplier);
         return "see that?";
-//        uartToActors = new UARTToGasSupplier();
-//        System.out.println("access to /uar2222tsend");
-//        if(uartToActors == null || true){
-//            System.out.println("----->0");
-//            uartToActors = new UARTToGasSupplier();
-//            System.out.println("----->1");
-//        }
-//        System.out.println("----->2");
-//        try {
-//            System.out.println("----->3");
-////            System.out.println("---->" + uartToGasSupplier.getI());
-//            uartToActors.sendToGasSupplier("seeee");
-//            System.out.println("----->4");
-//        }
-//        catch (IOException e){
-//            System.out.println("---> " + e);
-//        }
-//        catch (NullPointerException e){
-//            System.out.println("--->" + e);
-//        }
-//
-//        return "seee that ?";
+
     }
 
     @RequestMapping("/uartreceive")
     public String recceiveMessage() throws InterruptedException, IOException{
         uartToActors.getReceivedFromGasSupplierString();
-//        return "received !";
-//        if(uartToActors == null){
-//            System.out.println("----> renew the uartToGasSupplier instance");
-//            uartToActors = new UARTToGasSupplier();
-//        }
-//        try {
-//            String message = uartToGasSupplier.getReceivedFromGasSupplierString();
-//        }
-//        catch ( e){
-//            System.out.println("--->" + e);
-//        }
         String message = uartToActors.getReceivedFromGasSupplierString();
         System.out.println("---> got this message ?");
         return message;
-
     }
 }
